ea54325c6dd8220ec6775e332b809c2327bc9219
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-03-25 23:52:32 +0530

    js basics

---
+++ javascript basics/javascriptintro.js
@@ -0,0 +1,13 @@
+console.log("hello world");
+//console is intrepreter in js and log prints value inside it 
+let a =10;
+// let is used to declare variable 
+let result ="";
+for( let i = 0 ; i <a ; i++){
+    console.log(i)  ;
+}
+// semicolon is optional in js 
+const b=20;
+// const for constant value 
+console.log(Number.MAX_SAFE_INTEGER);  
+// print maximum largest number allowed in js 
