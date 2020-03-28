ebb6cdcad47835a83b7f04bcc8e347ea448b34ca
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-03-28 13:15:02 +0530

    js

---
+++ javascript basics/functions and equality.js
@@ -0,0 +1,19 @@
+function inc(x,y){
+console.log(x+1);
+}
+inc(4);
+inc('hello');
+// print hello1 
+inc(true);
+// prints 2 (true=1 1+1=2) 
+
+console.log(1=='1');
+// prints true because == is a non strict comparator 
+console.log(1==='1');
+// prints false because === is a strict comrator 
+
+console.log('11'-1)
+//prints 10 string is typecast to number
+console.log([]==""); 
+console.log([]==0);
+
