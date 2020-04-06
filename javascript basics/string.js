ce36b121515db2fe15fe039f9ac20daf231a9f3e
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-04-06 23:51:13 +0530

    strings js

--- javascript basics/string.js
+++ javascript basics/string.js
@@ -3,11 +3,13 @@ let b= "bansal"
 let c = `${a} ${b}`    //this is backticks and can use when u want to use "" and '' both in a string
 console.log(c)
 
-let string = "this is a string"
+let string = "this-is-a-string"
 //indexof can also be used to search a string (if ans is -1 then string is not present)
 console.log(string.indexOf("is"))    //gives position of is inside this
 console.log(string.lastIndexOf("is"))  // gives actuall postion of is
 //substring
 console.log(string.slice(0,6))   //in slice we can put negative values as well
 console.log(string.slice(-6,-4))   //from back
+
+console.log(string.substr(1,4))  // prints substrings from index 1 of 4length
 
