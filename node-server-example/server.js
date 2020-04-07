c4c432d1a4aa1adfde222c5e21964689cdc01056
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-04-07 23:48:14 +0530

    server changes

--- node-server-example/server.js
+++ node-server-example/server.js
@@ -5,6 +5,16 @@ server.get('/',function(req,res,next){
     res.send("hello world")
 })
 
+server.get('/greet/:tod/:name',function(req,res,next){
+    let tod="morning"
+    switch(req.params.tod){
+     case 'evening' : tod="evening ";break;
+     case 'morning' : tod="morning ";break;
+
+    }
+
+    res.send("Good "+tod + req.params.name)
+})
 server.listen(2121)
 
 
