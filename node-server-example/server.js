28cfc0775ba6489ae8c7e8a4ae9ed1e338c66a40
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-04-08 20:17:57 +0530

    server middlewares

--- node-server-example/server.js
+++ node-server-example/server.js
@@ -4,7 +4,7 @@ server.get('/',function(req,res,next){
     console.log("yo")
     res.send("hello world")
 })
-
+//params
 server.get('/greet/:tod/:name',function(req,res,next){
     let tod="morning"
     switch(req.params.tod){
@@ -15,6 +15,24 @@ server.get('/greet/:tod/:name',function(req,res,next){
 
     res.send("Good "+tod + req.params.name)
 })
+
+//query
+server.get('/greet/:tod',function(req,res,next){
+    let tod="morning"
+    switch(req.params.tod){
+     case 'evening' : tod="evening, ";break;
+     case 'morning' : tod="morning, ";break;
+
+    }
+
+    res.send("Good "+tod + req.query.name)
+})
+
+
+
+
+
+
 server.listen(2121)
 
 
---
+++ node-server-middleware/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const server = express()
+
+let m1=function(req,res,next){
+    console.log("we are in m1")
+    next()
+}
+let m2=function(req,res,next){
+    console.log("we are in m2")
+    next()
+}
+let m3=function(res,req,next){
+    console.log("we are in m3")
+    next()
+}
+
+server.use(m1)
+server.use(m2)
+server.get('/',function(req,res,next){
+    res.send("hello world")
+})
+
+server.use(m3)
+
+server.listen(3232)
