b592ea488fe32a4bded216a28e591ac8693d5e24
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-04-11 00:13:53 +0530

    todo list server creted assignment

---
+++ assignments/server-todo/list.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const router = express.Router
+const route = router()
+module.exports=route
+
+let list =[
+    {task:"play",status:"notdone"},
+    {task:"eat",status:"done"},
+    {task:"study",status:"notdone"},
+    {task:"listen_music",status:"done"},
+    {task:"go_out",status:"notdone"}
+]
+
+route.get('/',(req,res)=>{
+    res.send(list);
+})
+
+route.get('/:id',(req,res)=>{
+res.send(list[req.params.id])
+})
+
+route.post('/',(req,res)=>{
+    list.push({
+        task:req.body.task,
+        status:req.body.status
+    })
+    res.send(list)
+})
+
+route.delete('/:id',(req,res)=>{
+    delete list[req.params.id]
+    res.send(list)
+})
+
+route.patch('/:id',(req,res)=>{
+    list[req.params.id]={tsak:req.body.task,status:req.body.status}
+    res.send(list)
+})
---
+++ assignments/server-todo/todoserver.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const srv = express()
+
+srv.use(express.json())
+srv.use(express.urlencoded({extended:true}))
+
+const list_srv=require('./list')
+
+srv.use('/todos',list_srv)
+srv.use(function(req,res,next){
+    res.send("<h2>404 Page Not Found</h2>")
+})
+
+
+srv.listen(1212)
