d2038250126741c75e7e17f6cfe531242aed61a9
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-04-11 21:07:05 +0530

    jquery library added

---
+++ assignments/server-todo/todoserver.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const srv = express()
+
+srv.use(express.json())
+srv.use(express.urlencoded({ extended: true }))
+
+const list_srv = require('./routes/list')
+
+srv.get('/todos', list_srv)
+srv.use('/public', express.static(__dirname+"/public"))
+
+srv.use(function (req, res, next) {
+    res.send("<h2>404 Page Not Found</h2>")
+})
+
+
+srv.listen(1212)
