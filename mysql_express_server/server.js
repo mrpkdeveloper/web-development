32464dcc7611a12957401242174a0b5846d81566
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-04-17 00:21:26 +0530

    all error resolved in server.js in mysql_express_server

--- mysql_express_server/server.js
+++ mysql_express_server/server.js
@@ -1,19 +1,18 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-app.use(express)
-app.use(express.urlencoded({ extended: true }))
-app.set("veiw engine", "hbs")
+app.use(express.json())
+app.use(express.urlencoded({ extended : true }))
+app.set('view engine', 'hbs')
+app.set('views',__dirname + "/views")
 
 app.get('/', (req, res) => {
     res.render('persons', {                                        // this line render persons.hbs file with following fake data
         persons: [
-            { name: 'aditya', age: 13, city: 'delhi' },
-            { name: 'bhavy', age: 14, city: 'bhopal' }
+            { name: "aditya", age: 13, city: "delhi" },
+            { name: "bhavy", age: 14, city: "bhopal" }
         ]
     })
 })
 
-app.listen(4444, () => {
-    console.log('server started on http://locolhost:4444/')
-})
+app.listen(4231)
