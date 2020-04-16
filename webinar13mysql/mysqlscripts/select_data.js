a125c0d70defa14a841e77375509fd5f86d6af95
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-04-16 12:50:51 +0530

    select_data.js added

---
+++ webinar13mysql/mysqlscripts/select_data.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql2')
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    database: 'mytestdb',
+    user: 'myuser',
+    password: 'mypass'
+
+})
+
+connection.query(
+    `SELECT * FROM pearsons`,
+    function (err, rows,cols) {
+        if (err) {
+            console.error(err)
+        } else {
+            console.log(rows)
+            console.log(cols)
+        }
+        connection.close()
+    }
+
+)
