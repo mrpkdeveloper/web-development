99d4b714792f01d8cf64de57ae7167b527e869e8
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-04-20 23:32:07 +0530

    .

--- mysql_express_server/public_static/script.js
+++ mysql_express_server/public_static/script.js
@@ -1,5 +1,14 @@
 $(
-    function(){
+    function () {
+        let input_name = $('#name')
+        let input_age = $('#age')
+        let input_city = $('#city')
+        let btn_submit = $('#submit')
+        let table = $('#persons')
 
+
+        btn_submit.click(function(){
+            console.log("button clicked")
+        })
     }
 )
--- mysql_express_server/routes/api.js
+++ mysql_express_server/routes/api.js
@@ -1,5 +1,23 @@
 const route = require('express').Router()
+//this route in on "/api/"path
 
+const db = require('../db')
+
+route.get('/',function(req,res){
+    //send all the persons as an array here
+    db.getallperson()
+    .then((persons)=>res.send(persons))
+    .catch((err)=>res.send({error: err}))
+
+})
+route.post('/',function(req,res){
+   //add the new persons   
+   db.addpersons(req.body.name,req.body.age,req.body.city)
+   .then(()=>res.redirect('/api/'))
+   .catch((err)=>res.send({error: err}))
+
+
+})
 
 exports = module.exports={
     route
