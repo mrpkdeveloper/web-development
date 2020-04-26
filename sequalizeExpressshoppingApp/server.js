ba6f9c29625c0688db2843a24fee6cc94c469020
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-04-26 20:44:09 +0530

    .

--- sequalizeExpressshoppingApp/server.js
+++ sequalizeExpressshoppingApp/server.js
@@ -7,5 +7,8 @@ app.use(express.urlencoded({extended:true}))
 //the above lines are for handling post requests to the server
 
 app.use('/', express.static(path.join(__dirname, 'public')))  //this file executes index.html in public folder
+//the above line used to declare the folowing folder as static 
+//static files can be accessed by anoyne 
+
 app.use('/api', require('./routes/api').route)  //this line execute index.js in api folder
 app.listen(1111, () => { console.log('server started at http://localhost:1111') })
