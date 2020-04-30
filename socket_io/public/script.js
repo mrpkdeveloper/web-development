196272ec9ee5697a8e72c4f738a10cd1f89bfde6
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-04-30 23:51:35 +0530

    som error

--- socket_io/public/script.js
+++ socket_io/public/script.js
@@ -1,1 +1,26 @@
-console.log('hello');
+let socket = io()
+socket.on('connected', () => {
+    console.log("connected " + socket.id)
+})
+
+$(
+    function () {
+
+        let input = $('#input')
+        let submit = $('#submit')
+        let msglist = $('#msglist')
+
+
+        submit.click(function () {
+
+            socket.emit('send_msg ', { msg: input.val() })
+
+        })
+
+        socket.on('recv_msg', function (data) {
+            msglist.append($('<li>' + data.msg + '</li>'))
+        })
+
+    }
+)
+//runs when page is loaded completely
--- socket_io/server.js
+++ socket_io/server.js
@@ -1,11 +1,23 @@
 const express = require('express')
-const app = express()
 const path = require('path')
+const socketio = require('socket.io')
+const http = require('http')
+
+
+const app = express()
+const server = http.createServer(app)   //well here we use this http stuff to use our app and server on same local host
+const io = socketio(server)
 
 
 //on opening server index.html file will execute in public folder 
-app.use('/',express.static(path.join(__dirname,'public')))
+app.use('/', express.static(path.join(__dirname, 'public')))
 
-app.listen(1234, () => {
-    console.log('server open at http://localhost:1234')
+io.on('connection', (socket) => {
+    console.log("new socket formed at " + socket.id)
+    socket.emit('connected')
+    socket.on('send_msg' , (data)=>{
+        io.emit('recv_msg ',data)       // send backs data to the client
+    })
 })
+
+server.listen(1234, () => console.log('server open at http://localhost:1234'))
