8cd6ddc2e83f3d1db4c3ecf4f2edbf927d57d56d
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-05-22 22:25:44 +0530

    res.sendfile path issue resolved

--- development/node_bascics(fs)-todo-server/todo-server/server_practice.js
+++ development/node_bascics(fs)-todo-server/todo-server/server_practice.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 let pageview = 0
 
@@ -7,7 +8,9 @@ let pageview = 0
 app.get('/', (req, res) => {
     // res.send("<h1>hello world</h1>")  //we can also pass html in res.send 
     //we have to send proper file path 
-    res.sendFile(__dirname + '/helloworld.html')
+    // res.sendFile(__dirname + '/helloworld.html')
+    // res.sendFile('../public/holi.html', { root: __dirname })
+    res.sendFile('holi.html', { root: path.join(__dirname, '../public') });
     // console.log(req.url)
 
     pageview++
