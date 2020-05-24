ea3137b82f486a616d8ede273274ee66e9e87bc0
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-05-24 13:59:37 +0530

    strings learned in js

--- development/javascript basics/string.js
+++ development/javascript basics/string.js
@@ -1,15 +1,23 @@
 let a = "prateek"
-let b= "bansal"
+let b = "bansal"
+
+//these ${} are templates
 let c = `${a} ${b}`    //this is backticks and can use when u want to use "" and '' both in a string
 console.log(c)
+console.log(c.length)
 
-let string = "this-is-a-string"
+
+let string = "this-is-a-string-is"
 //indexof can also be used to search a string (if ans is -1 then string is not present)
 console.log(string.indexOf("is"))    //gives position of is inside this
-console.log(string.lastIndexOf("is"))  // gives actuall postion of is
-//substring
-console.log(string.slice(0,6))   //in slice we can put negative values as well
-console.log(string.slice(-6,-4))   //from back
+console.log(string.lastIndexOf("is"))
 
-console.log(string.substr(1,4))  // prints substrings from index 1 of 4length
+//substring
+console.log(string.slice(0, 6))   //in slice we can put negative values as well
+console.log(string.slice(-6, -4))   //from back
+
+// prints substrings from index 1 of 4length
+console.log(string.substr(1, 4))
+console.log(string.substr(-5, 4))
+
 
