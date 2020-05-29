de5aae723e85f56a6a381dc4ddcb0440ce12f984
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-05-29 13:10:30 +0530

    set timeout function

--- development/javascript basics/async_to_promises.js
+++ development/javascript basics/async_to_promises.js
@@ -1,28 +1,50 @@
-function sometask(callback) {
-    console.log("we are doing some task")
-    setTimeout(function () {
-        callback();
+// function sometask(callback) {
+//     console.log("we are doing some task")
+//     setTimeout(function () {
+//         callback();
+//     }, 100);
+// }
+
+
+// sometask(function () {
+//     console.log("we did something")
+// })
+
+
+// //now same thing with promise
+
+// function sometaskpromise() {
+//     return new Promise(function (resolve, reject) {
+//         console.log("we are doing some task")
+//         setTimeout(() => {
+//             resolve();
+//         }, 100);
+//     })
+// }
+
+// sometaskpromise().then(function () {               //then is resolve function 
+//     console.log("we did something")
+
+// })
+
+
+
+//******set timeout function***************
+console.log("******set timeout function***************")
+function fakedownload(done) {
+
+    setTimeout(() => {
+        let downloaded_data = "downloaded data get from net"
+        done(downloaded_data)
     }, 3000);
 }
 
-
-sometask(function () {
-    console.log("we did something")
+//on calling this function first setimeout to 3000 milisecnds
+//then done function is called 
+fakedownload(function (data) {
+    console.log("we downloaded a file which has this data =====> ")
+    console.log(data)
 })
 
-
-//now same thing with promise
-
-function sometaskpromise() {
-    return new Promise(function (resolve, reject) {
-        console.log("we are doing some task")
-        setTimeout(() => {
-            resolve();
-        }, 3000);
-    })
-}
-
-sometaskpromise().then(function(){               //then is resolve function 
-    console.log("we did something")
-
-})
+//******set timeout function***************
+console.log("******promises***************")
