88627a81f702da16b7f5adbc85ea7ca6c9cdb357
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-05-30 12:40:26 +0530

    call stack

---
+++ development/javascript basics/function call and stack.js
@@ -0,0 +1,21 @@
+function ceo() {
+    console.log("lets make a dent in universe")
+    cto()
+}
+function cto() {
+    console.log("lets make a gamechanging product")
+    vpeng()
+}
+function vpeng() {
+    console.log("lets make a javascript framework")
+    techlead()
+}
+function techlead() {
+    console.log("lets port angular to typescript")
+    developer()
+}
+function developer() {
+    console.log("copy code from stack overflow")
+}
+
+ceo()
