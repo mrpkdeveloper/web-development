6806bd92fd1a02bd2b03235286d7e4491a215f8a
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-06-24 23:30:40 +0530

    inheritance and super constructor

--- development/javascript basics/es6_classes.js
+++ development/javascript basics/es6_classes.js
@@ -6,6 +6,18 @@ class person {
     }
 }
 
+class student extends person {
+    constructor(name, age, school) {
+        //super calls the parent class constructor
+        super(name, age) //we can use this only if we use super in extended class
+        this.school = school
+
+    }
+}
+
 let p = new person("harry", 30)
 console.log(p)
-p() //gives error beacuse pis not a function it behaves like a class 
+// console.log(typeof (p)) //object
+// p() //gives error beacuse pis not a function it behaves like a class
+let s = new student("ron", 20, "hogwarts")
+console.log(s)
