9d28a598fe22f2f14ddbba7d22a4e92d340e7086
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-06-25 23:30:31 +0530

    concurency on a single thread

---
+++ development/javascript basics/concurency.js
@@ -0,0 +1,18 @@
+function hellosayer(times, name) {
+    let count = 0
+    let loopid = setInterval(() => {
+        count++
+        console.log("hello " + name)
+        if (count == times) {
+            clearInterval(loopid)
+        }
+
+
+    }, 1000)
+
+}
+
+hellosayer(3, "prateek")
+hellosayer(2, "arnav")
+//so basically when prateek prints it waits for 1
+// sec and in that time other call runs and print arnav
