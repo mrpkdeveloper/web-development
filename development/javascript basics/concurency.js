71b226e8aa8055112b41c1f4be9d4311a102b572
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-06-26 13:08:02 +0530

    promises instead of callbacks for concurency

--- development/javascript basics/concurency.js
+++ development/javascript basics/concurency.js
@@ -12,8 +12,40 @@ function hellosayer(times, name) {
 
 }
 
-hellosayer(3, "prateek")
-hellosayer(2, "arnav")
+// hellosayer(3, "prateek")
+// hellosayer(2, "arnav")
 //so basically when prateek prints it waits for 1
 // sec and in that time other call runs and print arnav
 //these two calls are not running parallely
+
+
+
+//concurency using promises
+// console.log("***********concurency using promises*************")
+function hellosayer2(times, name) {
+    return new Promise((resolve, reject) => {
+        let count = 0
+        let loopid = setInterval(() => {
+            count++
+            console.log("hello " + name)
+            if (count == times) {
+                clearInterval(loopid)
+                resolve()
+            }
+
+
+        }, 1000)
+    })
+
+
+}
+
+//concurently
+// hellosayer2(2, "arnav")
+// .then(hellosayer2(2, "prateek"))
+// .then(hellosayer2(2, "varun"))
+
+//sequentially
+hellosayer2(2, "arnav")
+    .then(() => hellosayer2(2, "prateek"))
+    .then(() => hellosayer2(2, "varun"))
