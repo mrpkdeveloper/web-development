ff9690c6ee29977095fdb047fdf4fbad706c5ae4
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-06-27 23:16:44 +0530

    readingfiles assignment

--- development/nodejs/readfile.js
+++ development/nodejs/readfile.js
@@ -1,6 +1,38 @@
 const fs = require('fs')
+function readdata() {
+    return new Promise((resolve, reject) => {
+        fs.readFile(__dirname + '/mytext.txt', function (err, data) {
+            if (err) throw err
+            s = data.toString().split(",")
+            resolve(s)
+            // console.log(s)
+        })
 
-fs.readFile(__dirname + '/mytext.txt', function (err, data) {
-    if (err) throw err
-    console.log(data.toString())
-})
+    })
+
+}
+
+readdata()
+    .then((a) => {
+        for (let j = 0; j < a.length; j++) {
+            var c = 0;
+            for (let i = j; i < a.length; i++) {
+                if (a[j] == a[i]) {
+                    c++
+                }
+            }
+            console.log(a[j] + " count : " + c)
+        }
+
+
+    })
+
+
+
+
+
+
+
+// let a = "a,b,c"
+// let b = a.split(",")
+// console.log(b)
