fb15365413be9b602b26c4201ccc9dd33f2bb488
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-07-12 18:52:30 +0530

    remove shoping app and created as a seperate project

--- development/sequalizeExpressshoppingApp/public/cart.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-let buy_btn = $('#button-1') 
-
-buy_btn.click(function(){
-    console.log("button clicked")
-})
-})
--- development/sequalizeExpressshoppingApp/routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const route = require('express').Router()
-
-route.use('/users',require('./users'))
-route.use('/products',require('./products'))
-route.use('/cart',require('./cart'))
-exports = module.exports={
-    route
-}
